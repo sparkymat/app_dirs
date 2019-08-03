28cc75eaa973e7ae48b9aaecdb7e28d8806ea57c
Ajith Hussain <csy0013@googlemail.com>
2019-08-03 17:43:56 +1000

    Added tests

--- appdir_test.go
+++ appdir_test.go
@@ -1,6 +1,8 @@
 package appdir
 
 import (
+	"fmt"
+	"os"
 	"runtime"
 	"testing"
 
@@ -18,7 +20,36 @@ func TestDataDir(t *testing.T) {
 	if runtime.GOOS == "windows" {
 		assert.Equal(t, "windows", dir)
 	} else if runtime.GOOS == "linux" {
-		assert.Equal(t, "linux", dir)
+		homeDir := os.Getenv("HOME")
+		assert.Equal(t, fmt.Sprintf("%v/.local/share/TestApp", homeDir), dir)
+	} else if runtime.GOOS == "darwin" {
+		assert.Equal(t, "darwin", dir)
+	}
+}
+
+func TestConfigDir(t *testing.T) {
+	dir, err := appInfo.ConfigRoot()
+	assert.Nil(t, err)
+
+	if runtime.GOOS == "windows" {
+		assert.Equal(t, "windows", dir)
+	} else if runtime.GOOS == "linux" {
+		homeDir := os.Getenv("HOME")
+		assert.Equal(t, fmt.Sprintf("%v/.config/TestApp", homeDir), dir)
+	} else if runtime.GOOS == "darwin" {
+		assert.Equal(t, "darwin", dir)
+	}
+}
+
+func TestCacheDir(t *testing.T) {
+	dir, err := appInfo.CacheRoot()
+	assert.Nil(t, err)
+
+	if runtime.GOOS == "windows" {
+		assert.Equal(t, "windows", dir)
+	} else if runtime.GOOS == "linux" {
+		homeDir := os.Getenv("HOME")
+		assert.Equal(t, fmt.Sprintf("%v/.cache/TestApp", homeDir), dir)
 	} else if runtime.GOOS == "darwin" {
 		assert.Equal(t, "darwin", dir)
 	}
