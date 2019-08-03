b8682c5a810cf82b8fdba95375d48132dd43a27d
Ajith Hussain <csy0013@googlemail.com>
2019-08-03 17:47:00 +1000

    Added comments

--- path.go
+++ path.go
@@ -4,11 +4,13 @@ import (
 	"path/filepath"
 )
 
+// AppInfo contains information required about the application
 type AppInfo struct {
 	Name   string
 	Author string
 }
 
+// DataPath returns the absolute path for the specified path inside the app's data folder
 func (info AppInfo) DataPath(path string) (string, error) {
 	root, err := info.DataRoot()
 	if err != nil {
@@ -18,6 +20,7 @@ func (info AppInfo) DataPath(path string) (string, error) {
 	return filepath.Join(root, path), nil
 }
 
+// ConfigPath returns the absolute path for the specified path inside the app's config folder
 func (info AppInfo) ConfigPath(path string) (string, error) {
 	root, err := info.ConfigRoot()
 	if err != nil {
@@ -27,6 +30,7 @@ func (info AppInfo) ConfigPath(path string) (string, error) {
 	return filepath.Join(root, path), nil
 }
 
+// CachePath returns the absolute path for the specified path inside the app's cache folder
 func (info AppInfo) CachePath(path string) (string, error) {
 	root, err := info.CacheRoot()
 	if err != nil {
